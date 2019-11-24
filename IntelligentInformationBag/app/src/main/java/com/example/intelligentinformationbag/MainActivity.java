d6fa00f4fb1f5e11ca136bbc3858fedab44ef95f
フライパン <Flying-pan@furaipan-no-MacBook-Pro.local>
2019-11-25 08:22:15 +0900

    Add multiple screen logic. (https://akira-watson.com/android/activity-1.html)

---
+++ IntelligentInformationBag/app/src/main/java/com/example/intelligentinformationbag/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.intelligentinformationbag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button sendButton = findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ IntelligentInformationBag/app/src/main/java/com/example/intelligentinformationbag/SubActivity.java
@@ -0,0 +1,25 @@
+package com.example.intelligentinformationbag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_sub);
+
+        Button returnButton = findViewById(R.id.return_button);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
