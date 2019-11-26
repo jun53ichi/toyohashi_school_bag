2b378dfb928befe449a25d7f1af438fd7af29bd5
フライパン <Flying-pan@furaipan-no-MacBook-Pro.local>
2019-11-27 08:28:18 +0900

    Add Key ivent logic in MainActivity.

--- MyApplication_brightness/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication_brightness/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,6 +13,8 @@ import android.widget.TextView;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.view.KeyEvent;
+import android.util.Log;
 
 import java.util.List;
 
@@ -110,43 +112,63 @@ public class MainActivity extends Activity implements SensorEventListener {
             str = "照度:" + event.values[0];
             textView3.setText(str);
 
-        // 照度値よって明るさ値設定.
-        float brightness = 0.1f;
-        if(sensorValue < 100.0){
-            // 町の街灯下：100.
-            // これ以下を「暗い状況」とする.
-            brightness = 1.0f;
-        } else if (sensorValue > 1000.0){
-            // 大体部屋の明るさが1000未満？.
-            // なのでそれ以上は昼間の戸外などの明るい場所とする.
-            brightness = 0.5f;
-        } else {
-            // 明るい部屋の中.
-            brightness = 0.1f;
+            // 照度値よって明るさ値設定.
+            float brightness = 0.1f;
+            if(sensorValue < 100.0){
+                // 町の街灯下：100.
+                // これ以下を「暗い状況」とする.
+                brightness = 1.0f;
+            } else if (sensorValue > 1000.0){
+                // 大体部屋の明るさが1000未満？.
+                // なのでそれ以上は昼間の戸外などの明るい場所とする.
+                brightness = 0.5f;
+            } else {
+                // 明るい部屋の中.
+                brightness = 0.1f;
+            }
+
+            // Windowの明るさ設定.
+            WindowManager.LayoutParams lp = getWindow().getAttributes();
+            lp.screenBrightness = brightness;
+            getWindow().setAttributes(lp);
+
+            // 変更後の輝度表示.
+            // Windowの明るさ取得.
+            float value = lp.screenBrightness;
+            // systemの明るさを取得.
+            int sValue = 0;
+            try {
+                sValue = getInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS);
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            // 画面表示
+            String text2 = "new brightness : " + String.valueOf(value) + "(system : " + String.valueOf(sValue) + ")";
+            textView2.setText(text2);
         }
+    }
 
-        // Windowの明るさ設定.
-        WindowManager.LayoutParams lp = getWindow().getAttributes();
-        lp.screenBrightness = brightness;
-        getWindow().setAttributes(lp);
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        Log.v("KeyDown", "KeyCode=" + keyCode);
+        return super.onKeyDown(keyCode, event);
+    }
 
-        // 変更後の輝度表示.
-        // Windowの明るさ取得.
-        float value = lp.screenBrightness;
-        // systemの明るさを取得.
-        int sValue = 0;
-        try {
-            sValue = getInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS);
-        } catch (Settings.SettingNotFoundException e) {
-            e.printStackTrace();
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        // TODO Auto-generated method stub
+        String action="";
+
+        switch(event.getAction()) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                action = "Key Vol Up";
+                break;
         }
-
-        // 画面表示
-        String text2 = "new brightness : " + String.valueOf(value) + "(system : " + String.valueOf(sValue) + ")";
-        textView2.setText(text2);
-
-     }
-}
+        Log.v("KeyEvent", action + ": KeyCode=" + event.getKeyCode());
+        return super.dispatchKeyEvent(event);
+    }
 
 /*  ボタン処理.
     public void onClick(View view) {
